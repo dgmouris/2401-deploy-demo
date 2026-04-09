7fb885e921efdb3f5d19cda8bbe58e6521f68214
Daniel Mouris <dgmouris@gmail.com>
2026-04-08 21:00:19 -0600

    test

--- announcements_project/urls.py
+++ announcements_project/urls.py
@@ -21,13 +21,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('web.urls')),
+    path("", include("web.urls")),
     path("admin/", admin.site.urls),
-    path('accounts/', include('core.urls')),  # registration view added!
-    path('announcements/', include('announcements.urls')),  # announcements app urls
-    path('profiles/', include('profiles.urls')),  # profiles app urls
-    path('courses/', include('courses.urls')),  # courses app urls
+    path("accounts/", include("core.urls")),  # registration view added!
+    path("announcements/", include("announcements.urls")),  # announcements app urls
+    path("profiles/", include("profiles.urls")),  # profiles app urls
+    path("courses/", include("courses.urls")),  # courses app urls
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
